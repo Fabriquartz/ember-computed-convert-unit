967d61c455bba59ab8f0c299ad6d2d9f52a42a6f
Chantal Broeren <chantal.broeren@fabriquartz.com>
2015-07-20 13:17:01 +0200

    add convert-unit index file and tests

---
+++ addon/index.js
@@ -0,0 +1,58 @@
+import Ember from 'ember';
+
+const CONVERSION_MAP = {
+  // Please sort on alphabetic order, thank you!
+  'centimeters': {
+    'meters':        function(value) { return value / 100.0; },
+    'millimeters':   function(value) { return value * 10.0; }
+  },
+  'feet': {
+    'teu':           function(value) { return value / 20.0; }
+  },
+  'hours': {
+    'milliseconds':  function(value) { return value * 3600000; },
+    'minutes':       function(value) { return value * 60.0; },
+    'seconds':       function(value) { return value * 3600.0; }
+  },
+  'kilos': {
+    'tons':          function(value) { return value / 1000.0; }
+  },
+  'meters': {
+    'centimeters':   function(value) { return value * 100; },
+    'millimeters':   function(value) { return value * 1000; }
+  },
+  'millimeters': {
+    'centimeters':   function(value) { return value / 10.0; },
+    'meters':        function(value) { return value / 1000.0; }
+  },
+  'milliseconds': {
+    'hours':         function(value) { return value / 3600000.0; },
+    'minutes':       function(value) { return value / 60000.0; },
+    'seconds':       function(value) { return value / 1000.0; }
+  },
+  'minutes': {
+    'hours':         function(value) { return value / 60.0; },
+    'milliseconds':  function(value) { return value / 60000.0; },
+    'seconds':       function(value) { return value * 60.0; }
+  },
+  'teu': {
+    'feet':          function(value) { return value * 20.0; }
+  },
+  'tons': {
+    'kilos':         function(value) { return value * 1000; }
+  }
+};
+
+export default function(value, fromUnit, toUnit) {
+  if (value === null) { return value; }
+  if (typeof value !== 'number') {
+    throw new Ember.Error('Input to conversion function must be a number');
+  }
+
+  if (CONVERSION_MAP[fromUnit] && CONVERSION_MAP[fromUnit][toUnit]) {
+    return CONVERSION_MAP[fromUnit][toUnit](value);
+  } else {
+    throw new Ember.Error('Conversion from ' + fromUnit + ' to ' +
+                                               toUnit + 'not available');
+  }
+}
---
+++ tests/unit/convert-unit-computed-test.js
@@ -0,0 +1,23 @@
+import qUnit  from 'qunit';
+import { test } from 'ember-qunit';
+
+import Ember from 'ember';
+import convertUnitComputed from 'ember-railio-convert-unit/computed';
+
+qUnit.module('Unit: Convert Unit Computed Property');
+
+const Rock = Ember.Object.extend({
+  kilos: null,
+  tons:  convertUnitComputed('kilos', 'kilos', 'tons')
+});
+
+test('converts the units', function(assert) {
+  const rock = Rock.create({ kilos: 2500 });
+  assert.equal(rock.get('tons'), 2.5);
+});
+
+test('can set the converted unit', function(assert) {
+  const rock = Rock.create({ kilos: 1000 });
+  rock.set('tons', 4.8);
+  assert.equal(rock.get('kilos'), 4800);
+});
