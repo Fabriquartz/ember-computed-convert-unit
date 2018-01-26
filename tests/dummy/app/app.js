0090eb2ddf3e1b139ccc766b0d9cd21179882b5e
Heng <tenn.luo@gmail.com>
2018-01-26 16:21:46 +0100

    update ember to v2.18

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,6 +1,6 @@
 import Application      from '@ember/application';
 import Resolver         from './resolver';
-import loadInitializers from 'ember/load-initializers';
+import loadInitializers from 'ember-load-initializers';
 import config           from './config/environment';
 
 const App = Application.extend({
