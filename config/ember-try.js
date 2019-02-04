d972be2a83e041731898550612703611ead60c0e
Pieter van Laatum <pieter.van.laatum@fabriquartz.com>
2019-02-04 14:57:28 +0100

    Upgrade Ember to version 3.7

--- config/ember-try.js
+++ config/ember-try.js
@@ -38,6 +38,15 @@ module.exports = function() {
           bower: {}
         },
         {
+          name: 'ember-lts-3.4',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.4.0'
+            }
+          },
+          bower: {}
+        },
+        {
           name: 'ember-release',
           npm:  {
             devDependencies: {
