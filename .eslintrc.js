16f560bf9213289e00af914e27d5918a71b6fe7f
Pieter van Laatum <pieter.van.laatum@fabriquartz.com>
2021-10-20 16:58:47 +0200

    Fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,88 +1,91 @@
 'use strict';
 
 module.exports = {
-  root:          true,
-  parser:        'babel-eslint',
+  root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion:  2018,
-    sourceType:   'module',
+    ecmaVersion: 2018,
+    sourceType: 'module',
     ecmaFeatures: {
       legacyDecorators: true,
     },
   },
-  extends: ['eslint:recommended', 'plugin:ember/recommended', 'plugin:prettier/recommended'],
-  env:     {
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
     embertest: true,
-    browser:   true,
-    es6:       true
+    browser: true,
+    es6: true,
   },
   plugins: ['align-assignments', 'arca', 'ember'],
-  rules:   {
-    'align-assignments/align-assignments':             ['warn', { requiresOnly: false }],
-    'arca/import-align':                               'error',
-    'arca/newline-after-import-section':               'error',
-    'arca/melted-constructs':                          'error',
-    'arca/import-ordering':                            'error',
-    'array-bracket-spacing':                           'off',
-    camelcase:                                         ['error'],
-    'ember/no-jquery':                                 'warn',
-    'ember/use-ember-data-rfc-395-imports':            ['error'],
-    'ember/new-module-imports':                        ['error'],
-    'ember/no-function-prototype-extensions':          ['error'],
-    'ember/no-old-shims':                              ['error'],
-    'ember/no-unnecessary-route-path-option':          ['error'],
+  rules: {
+    'align-assignments/align-assignments': ['warn', { requiresOnly: false }],
+    'arca/import-align': 'error',
+    'arca/newline-after-import-section': 'error',
+    'arca/melted-constructs': 'error',
+    'arca/import-ordering': 'error',
+    'array-bracket-spacing': 'off',
+    camelcase: ['error'],
+    'ember/no-jquery': 'warn',
+    'ember/use-ember-data-rfc-395-imports': ['error'],
+    'ember/new-module-imports': ['error'],
+    'ember/no-function-prototype-extensions': ['error'],
+    'ember/no-old-shims': ['error'],
+    'ember/no-unnecessary-route-path-option': ['error'],
     'ember/no-unnecessary-service-injection-argument': ['error'],
-    'ember/require-computed-macros':                   ['error'],
+    'ember/require-computed-macros': ['error'],
 
     'ember/avoid-leaking-state-in-ember-objects': ['warn'],
-    'ember/computed-property-getters':            ['error'],
+    'ember/computed-property-getters': ['error'],
 
-    'ember/jquery-ember-run':                                   ['error'],
-    'ember/no-arrow-function-computed-properties':              ['error'],
-    'ember/no-capital-letters-in-routes':                       ['error'],
-    'ember/no-deeply-nested-dependent-keys-with-each':          ['error'],
-    'ember/no-duplicate-dependent-keys':                        ['error'],
-    'ember/no-ember-super-in-es-classes':                       ['error'],
-    'ember/no-ember-testing-in-module-scope':                   ['error'],
+    'ember/jquery-ember-run': ['error'],
+    'ember/no-arrow-function-computed-properties': ['error'],
+    'ember/no-capital-letters-in-routes': ['error'],
+    'ember/no-deeply-nested-dependent-keys-with-each': ['error'],
+    'ember/no-duplicate-dependent-keys': ['error'],
+    'ember/no-ember-super-in-es-classes': ['error'],
+    'ember/no-ember-testing-in-module-scope': ['error'],
     'ember/no-incorrect-calls-with-inline-anonymous-functions': ['error'],
-    'ember/no-invalid-debug-function-arguments':                ['error'],
-    'ember/no-side-effects':                                    ['warn'],
-    'ember/require-super-in-init':                              ['error'],
-    'ember/require-computed-property-dependencies':             ['error'],
-    'ember/use-brace-expansion':                                ['warn'],
+    'ember/no-invalid-debug-function-arguments': ['error'],
+    'ember/no-side-effects': ['warn'],
+    'ember/require-computed-property-dependencies': ['error'],
+    'ember/use-brace-expansion': ['warn'],
 
     'object-curly-spacing': ['error', 'always'],
 
-    'no-console':  ['error', { allow: ['error'] }],
-    quotes:        ['error', 'single', { allowTemplateLiterals: true }],
+    'no-console': ['error', { allow: ['error'] }],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
     'brace-style': ['error', '1tbs', { allowSingleLine: true }],
-    indent:        [
+    indent: [
       'error',
       2,
       {
-        FunctionExpression:  { body: 1, parameters: 'first' },
+        FunctionExpression: { body: 1, parameters: 'first' },
         FunctionDeclaration: { body: 1, parameters: 'first' },
-        CallExpression:      { arguments: 'off' },
-        MemberExpression:    'off',
-        ObjectExpression:    'off',
-        ImportDeclaration:   'first',
-        ArrayExpression:     'first',
-        ignoredNodes:        ['ConditionalExpression']
-      }
+        CallExpression: { arguments: 'off' },
+        MemberExpression: 'off',
+        ObjectExpression: 'off',
+        ImportDeclaration: 'first',
+        ArrayExpression: 'first',
+        ignoredNodes: ['ConditionalExpression'],
+      },
     ],
     'key-spacing': [
       'error',
       {
         multiLine: { beforeColon: false },
-        align:     { beforeColon: false, on: 'value' }
-      }
+        align: { beforeColon: false, on: 'value' },
+      },
     ],
     'max-len': ['error', { code: 90, ignorePattern: '^\\s*(import|test)' }],
 
     // 'multiline-ternary':       ['error', 'never'],
     'max-statements-per-line': 'off',
-    'new-cap':                 'off',
-    'operator-linebreak':      'off'
+    'new-cap': 'off',
+    'operator-linebreak': 'off',
   },
   overrides: [
     // node files
